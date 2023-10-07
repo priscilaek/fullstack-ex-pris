75e2ff52d9975a01f0b2a680ea2ccaea034afbf9
Miguel Nieva <m@mikenieva.com>
2023-10-07 10:46:53 -0600

    Introducción de dotenv

--- server/index.js
+++ server/index.js
@@ -1,42 +1,38 @@
 // 1. IMPORTACIONES
 import express from "express"
 import cors from "cors"
-
+import dotenv from "dotenv"
 
 // 2. INICIALIZADORES
 const app = express()
 app.use(cors())
 app.use(express.json())
-
-
+dotenv.config()
 
 // OPCIONAL (LUEGO SE INTERCAMBIARÁ POR UNA BASE DE DATOS)
 let data = [
-    {
-        id: 1,
-        nombre: "Mike"
-    },
-    { 
-        id: 2,
-        nombre: "Ramiro"
-    }
+  {
+    id: 1,
+    nombre: "Mike",
+  },
+  {
+    id: 2,
+    nombre: "Ramiro",
+  },
 ]
-
 
 // 3. RUTAS
 
 // PROD: https://midominio.com/
 // DEV: localhost:3005/
-app.get("/", () => {
-    res.json({
-        message: "Datos obtenidos con éxito.",
-        data: data
-
-    })
-
+app.get("/", (req, res) => {
+  res.json({
+    message: "Datos obtenidos con éxito.",
+    data: data,
+  })
 })
 
-
-
 // 4. LEVANTAMIENTO DEL SERVIDOR
-app.listen("3005", () => console.log("Servidor está activo."))
+app.listen(process.env.BASE_URL_PORT, () =>
+  console.log("Servidor está activo.")
+)
