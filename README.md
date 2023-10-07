f20d8cc9432d0c2792948e63a8214fca5a8fd219
Miguel Nieva <m@mikenieva.com>
2023-10-07 10:13:38 -0600

    added .gitignore on server

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Instalación de proyecto
+
+
+
+## SERVER
+
+- Dependencias
+    - Express
+    - Nodemon
+    - CORS. Librería que permite abrir permisos a conexiones de terceros.
---
+++ server/index.js
@@ -0,0 +1,18 @@
+// 1. IMPORTACIONES
+import express from "express"
+import cors from "cors"
+
+
+// 2. INICIALIZADORES
+
+
+
+
+
+
+
+// 3. RUTAS
+
+
+
+// 4. LEVANTAMIENTO DEL SERVIDOR
