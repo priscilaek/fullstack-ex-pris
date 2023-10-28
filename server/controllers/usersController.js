a7f5007bf02f931fb60ab0fe52de5337c0466e20
Miguel Nieva <m@mikenieva.com>
2023-10-28 12:46:58 -0600

    Últimos cambios en creación de usuario

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,4 +1,5 @@
 import User from "./../models/User.js"
+import bcryptjs from "bcryptjs"
 
 const readAll = (req, res) => {
   res.json({
@@ -8,10 +9,18 @@ const readAll = (req, res) => {
 }
 
 const create = async (req, res) => {
-  const { name } = req.body
+  const { name, email, password } = req.body
+
+  // ESTABLECER EL NIVEL DE DIFICULTAD DE ENCRIPTAMIENTO DEL PASSWORD
+  const salt = await bcryptjs.genSalt(10)
+
+  // ENCRIPTAR EL PASSWORD
+  const hashedPassword = await bcryptjs.hash(password, salt)
 
   const newUser = await User.create({
     name,
+    email,
+    password: hashedPassword,
   })
 
   res.json({
--- server/models/User.js
+++ server/models/User.js
@@ -6,6 +6,15 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 })
 
 const User = mongoose.model("User", userSchema)
